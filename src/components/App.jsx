7d51a38dcf4cd1c53613a663529fc32860237c88
Suriyaa <76957372+suriyaaap13@users.noreply.github.com>
2022-04-11 13:37:47 +0530

    When new text is written into the input, its state should be saved

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [input, inputSetter] = useState("");
+
+  function handleChange(event) {
+    inputSetter(event.target.value);
+  }
+
+  // function handleClick(event){
+  //   inputSetter(prevValue =>{
+  //     if(prevValue.length>0){
+  //       let li = document.createElement("li");
+  //       li.textContent = prevValue;
+  //       document.getElementById('list-container').appendChild(li);
+  //     }
+  //   });
+  //   inputSetter("");
+  // }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
+        <input type="text" onChange={handleChange} value={input} />
         <button>
+          {/* onClick = {handleClick} */}
           <span>Add</span>
         </button>
       </div>
@@ -22,3 +40,4 @@ function App() {
 }
 
 export default App;
+// id = "list-container"
